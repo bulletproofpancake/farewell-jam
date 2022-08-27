b9ac6ad9fd19708ded558b6b12bb3a62e0876ad5
Jolo Gutierrez <57520402+bulletproofpancake@users.noreply.github.com>
2022-08-27 13:17:00 +0800

    camera, movement and animations now sync

--- Assets/Scripts/Player/ThirdPersonController.cs
+++ Assets/Scripts/Player/ThirdPersonController.cs
@@ -1,15 +1,17 @@
-using System;
+using FishNet.Object;
+using Cinemachine;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using FishNet.Component.Animating;
 
 namespace FJ.Player
 {
-    public class ThirdPersonController : MonoBehaviour
+    public class ThirdPersonController : NetworkBehaviour
     {
         // input fields
         private PlayerControls _playerControls;
         private InputAction _move;
-        
+
         // movement fields
         private Rigidbody _rb;
         [SerializeField] private float movementForce = 1f;
@@ -18,11 +20,25 @@ namespace FJ.Player
 
         [SerializeField] private Camera playerCamera;
         private Animator _animator;
+        private NetworkAnimator _networkAnimator;
+
+        public override void OnStartClient()
+        {
+            base.OnStartClient();
+            if (base.IsOwner && playerCamera == null)
+            {
+                playerCamera = Camera.main;
+                playerCamera.GetComponent<CinemachineFreeLook>().Follow = transform;
+                playerCamera.GetComponent<CinemachineFreeLook>().LookAt = transform;
+            }
+        }
+
         private void Awake()
         {
             _rb = GetComponent<Rigidbody>();
             _playerControls = new PlayerControls();
             _animator = GetComponent<Animator>();
+            _networkAnimator = GetComponent<NetworkAnimator>();
         }
 
         private void OnEnable()
@@ -31,7 +47,7 @@ namespace FJ.Player
             _move = _playerControls.Gameplay.Move;
             _playerControls.Gameplay.Enable();
         }
-        
+
         private void OnDisable()
         {
             _playerControls.Gameplay.Push.started -= DoPush;
@@ -40,9 +56,10 @@ namespace FJ.Player
 
         private void FixedUpdate()
         {
+            if (!base.IsOwner) return;
             _forceDirection += _move.ReadValue<Vector2>().x * GetCameraRight(playerCamera) * movementForce;
             _forceDirection += _move.ReadValue<Vector2>().y * GetCameraForward(playerCamera) * movementForce;
-            
+
             _rb.AddForce(_forceDirection, ForceMode.Impulse);
             _forceDirection = Vector3.zero;
 
@@ -53,7 +70,9 @@ namespace FJ.Player
             horizontalVelocity.y = 0f;
             if (horizontalVelocity.sqrMagnitude > maxSpeed * maxSpeed)
                 _rb.velocity = horizontalVelocity.normalized * maxSpeed + Vector3.up * _rb.velocity.y;
-            
+
+            _animator.SetFloat("speed", _rb.velocity.magnitude / maxSpeed);
+
             LookAt();
         }
 
@@ -61,13 +80,13 @@ namespace FJ.Player
         {
             Vector3 direction = _rb.velocity;
             direction.y = 0f;
-            
+
             if (_move.ReadValue<Vector2>().sqrMagnitude > 0.1f && direction.sqrMagnitude > 0.1f)
                 _rb.rotation = Quaternion.LookRotation(direction, Vector3.up);
             else
                 _rb.angularVelocity = Vector3.zero;
         }
-        
+
         private Vector3 GetCameraForward(Camera playerCam)
         {
             Vector3 forward = playerCamera.transform.forward;
@@ -84,7 +103,8 @@ namespace FJ.Player
 
         private void DoPush(InputAction.CallbackContext obj)
         {
-            _animator.SetTrigger("attack");
+            if (!base.IsOwner) return;
+            _networkAnimator.SetTrigger("attack");
         }
     }
 }
